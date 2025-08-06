2d9338ee4e43758e5200ffe9786ab25cc619d8e0
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-06 17:41:34 +0530

    initialized spring boot backend

---
+++ backend/src/test/java/com/moments/backend/MomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moments.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
