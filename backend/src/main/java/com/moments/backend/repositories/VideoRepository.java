e54412af7ad34653516299a3282fb50f60fd53eb
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-06 18:40:17 +0530

    created Repositories for Video and Playlist entities

---
+++ backend/src/main/java/com/moments/backend/repositories/VideoRepository.java
@@ -0,0 +1,13 @@
+package com.moments.backend.repositories;
+
+import com.moments.backend.entities.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, String> {
+
+    Optional<Video> findByTitle(String title);
+}
