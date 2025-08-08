e1dadc48fd21f3bf88f0965ec40f03d3f0540441
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-08 19:03:12 +0530

    enabled cors for all origins during development

--- backend/src/main/java/com/moments/backend/controllers/VideoController.java
+++ backend/src/main/java/com/moments/backend/controllers/VideoController.java
@@ -4,14 +4,12 @@ import com.moments.backend.entities.Video;
 import com.moments.backend.payload.CustomMessage;
 import com.moments.backend.services.VideoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/v1/videos")
+@CrossOrigin("*")
 public class VideoController {
 
     private final VideoService videoService;
