1671a6e64595a3eba51f8051138880fc75f30846
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-08 19:49:40 +0530

    changed Video get(String videoId) to Video get(UUID videoId) in VideoService interface

--- backend/src/main/java/com/moments/backend/services/VideoService.java
+++ backend/src/main/java/com/moments/backend/services/VideoService.java
@@ -5,10 +5,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 public interface VideoService {
 
-    Video get(String videoId);
+    Video get(UUID videoId);
 
     Video getByTitle(String title);
 
