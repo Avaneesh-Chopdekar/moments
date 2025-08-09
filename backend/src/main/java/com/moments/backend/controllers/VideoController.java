702ed1efbc8316b2850c83e10e9550be3583c852
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-09 17:26:15 +0530

    added streamVideoRange() method in VideoController class that sends video in byte chunks

--- backend/src/main/java/com/moments/backend/controllers/VideoController.java
+++ backend/src/main/java/com/moments/backend/controllers/VideoController.java
@@ -4,12 +4,19 @@ import com.moments.backend.entities.Video;
 import com.moments.backend.payload.CustomMessage;
 import com.moments.backend.services.VideoService;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.UUID;
 
 @RestController
@@ -68,6 +75,78 @@ public class VideoController {
                 .ok()
                 .contentType(MediaType.parseMediaType(contentType))
                 .body(resource);
+
+    }
+
+    @GetMapping("/stream/range/{videoId}")
+    public ResponseEntity<Resource> streamVideoRange(
+            @PathVariable("videoId") UUID videoId,
+            @RequestHeader(value = "Range", required = false) String range
+    ) {
+        Video video = videoService.get(videoId);
+        if (video == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        Path path = Paths.get(video.getFilePath());
+
+        Resource resource = new FileSystemResource(path.toFile());
+
+        String contentType = video.getContentType();
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        long fileLength = path.toFile().length();
+
+        if (range == null) {
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        }
+
+        long rangeStart, rangeEnd;
+        String[] values = range.replace("bytes=", "").split("-");
+        rangeStart = Long.parseLong(values[0]);
+        if (values.length > 1) {
+            rangeEnd = Long.parseLong(values[1]);
+        } else {
+            rangeEnd = fileLength - 1;
+        }
+
+        if (rangeEnd > fileLength - 1) {
+            rangeEnd = fileLength - 1;
+        }
+
+//        System.out.println("LOG: Range Start -" + rangeStart);
+//        System.out.println("LOG: Range End -" + rangeEnd);
+
+        InputStream inputStream;
+
+        try {
+            inputStream = Files.newInputStream(path);
+            inputStream.skip(rangeStart);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        long contentLength = rangeEnd - rangeStart + 1;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("X-Content-Type-Options", "nosniff");
+        headers.setContentLength(contentLength);
+
+        return ResponseEntity
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .headers(headers)
+                .contentType(MediaType.parseMediaType(contentType))
+                .contentLength(contentLength)
+                .body(new InputStreamResource(inputStream));
 
     }
 }
