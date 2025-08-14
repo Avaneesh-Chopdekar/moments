7d3384b3e84818586c9ad17c4d134bd8f250e858
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-14 10:34:51 +0530

    added three more methods findByTitleContainingIgnoreCaseAndVisibility(), findTop10ByOrderByViewsDesc(), findByVisibility() in VideoRepository interface

--- backend/src/main/java/com/moments/backend/repositories/VideoRepository.java
+++ backend/src/main/java/com/moments/backend/repositories/VideoRepository.java
@@ -1,14 +1,22 @@
 package com.moments.backend.repositories;
 
 import com.moments.backend.entities.Video;
+import com.moments.backend.enums.Visibility;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, UUID> {
 
     Optional<Video> findByTitle(String title);
+
+    Page<Video> findByTitleContainingIgnoreCaseAndVisibility(String title, Visibility visibility, Pageable pageable);
+    List<Video> findTop10ByOrderByViewsDesc();
+    Page<Video> findByVisibility(Visibility visibility, Pageable pageable);
 }
